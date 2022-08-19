c831adcb3fb47d9c8fb17443c443ce5ae654919b
crisvalhas <cristianrubiles@gmail.com>
2022-08-19 18:03:18 -0300

    finalStyles

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -19,18 +19,19 @@ export default function Banner () {
     return (
         <div className="Banner">
             <p className="banner-title">Nuestras frutas</p>
+            <div className='line'></div>
             <div className="banner-container">
-                <img title="Red Delicious" className="banner-imgs" src={icon_reddelicious} alt='icon_reddelicious' />
-                <img title="Granny Smith" className="banner-imgs" src={icon_grannysmith} alt='icon_grannysmith' />
-                <img title="Gala / Galaxy" className="banner-imgs" src={icon_galagalaxy} alt='icon_galagalaxy' />
-                <img title="Cripps Pink / Pink Lady" className="banner-imgs" src={icon_pinklady} alt='icon_pinklady' />
-                <img title="Abate Fetel" className="banner-imgs" src={icon_abatefetel} alt='icon_abatefetel' />
-                <img title="Beurre D'Anjou" className="banner-imgs" src={icon_beurredanjou} alt='icon_beurredanjou' />
-                <img title="Red Bartlett" className="banner-imgs" src={icon_redbartlett} alt='icon_redbartlett' />
-                <img title="Packham's Triumph" className="banner-imgs" src={icon_packhamstriumph} alt='icon_packhamstriumph' />
-                <img title="William's - Bartlett" className="banner-imgs" src={icon_williamsbartlett} alt='icon_williamsbartlett' />
+                <div className="Fhover"><img title="Red Delicious" className="banner-imgs" src={icon_reddelicious} alt='icon_reddelicious' /><p className="Banner-text">Red Delicious</p></div>
+                <div className="Fhover"><img title="Granny Smith" className="banner-imgs" src={icon_grannysmith} alt='icon_grannysmith' /><p className="Banner-text">Granny Smith</p></div>
+                <div className="Fhover"><img title="Gala / Galaxy" className="banner-imgs" src={icon_galagalaxy} alt='icon_galagalaxy' /><p className="Banner-text">Gala /Galaxy</p></div>
+                <div className="Fhover"><img title="Cripps Pink / Pink Lady" className="banner-imgs" src={icon_pinklady} alt='icon_pinklady' /><p className="Banner-text">Cripps Pink /Pink Lady</p></div>
+                <div className="Fhover"><img title="Abate Fetel" className="banner-imgs" src={icon_abatefetel} alt='icon_abatefetel' /><p className="Banner-text">Abate Fetel</p></div>
+                <div className="Fhover"><img title="Beurre D'Anjou" className="banner-imgs" src={icon_beurredanjou} alt='icon_beurredanjou' /><p className="Banner-text">Beurre D'Anjou</p></div>
+                <div className="Fhover"><img title="Red Bartlett" className="banner-imgs" src={icon_redbartlett} alt='icon_redbartlett' /><p className="Banner-text">Red Bartlett</p></div>
+                <div className="Fhover"><img title="Packham's Triumph" className="banner-imgs" src={icon_packhamstriumph} alt='icon_packhamstriumph' /><p className="Banner-text">Packham's Triumph</p></div>
+                <div className="Fhover"><img title="William's - Bartlett" className="banner-imgs" src={icon_williamsbartlett} alt='icon_williamsbartlett' /><p className="Banner-text">William's-Bartlett</p></div>
             </div>
-
+            <div className='line'></div>
         </div>
     )
 };
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,26 @@
-import "./Styles/Footer.css"
-export default function Footer () {
+import "./Styles/Footer.css";
+import C from "../assets/C.ico";
+import M from "../assets/M.ico";
+
+export default function Footer() {
     return (
         <div className="box">
-        <div className="footer">
-            <div >Ruta 151 Km 9, Cipolletti, Rio Negro, Argentina.</div>
-            <div >Mail: Contacto@puntayhacha.com.ar</div>
-        </div>
-        <div className="desing">
-        <div >(c) Cervi 2022 - All rights reserved.</div>
-        <div>Web desing: <a href="https://portfolio-ilqfe9r0l-crisvalhas.vercel.app/"> C </a> <a href="https://portfolio-git-main-usermauro.vercel.app/"> M </a></div> 
-        </div>
+            <div className="footer">
+                <div className="pre">
+                    <div>Ruta 151 Km 9, Cipolletti, Rio Negro, Argentina.</div>
+                    <div>Web design: 
+                        <a className="link" href="https://portfolio-ilqfe9r0l-crisvalhas.vercel.app/"> 
+                            <img className="Devico" src={C} alt="c"/>
+                        </a> 
+                        <a className="link" href="https://portfolio-git-main-usermauro.vercel.app/"> 
+                            <img className="Devico" src={M}  alt="M"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="desing">
+                <div >(c) Punta y Hacha 2022 - All rights reserved.</div>
+            </div>
         </div>
     )
 };
