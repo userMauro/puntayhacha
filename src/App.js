45367945023fa633ba33c5c58055c5a3c4a2a06e
usermauro <mauroncioci@gmail.com>
2024-04-04 15:10:19 -0300

    Small update & comment certifications

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Nav from "./Components/Nav";
 import Landing from "./Components/Landing";
 import Banner from "./Components/Banner";
 import AboutUs from "./Components/AboutUs";
-import Certificates from "./Components/Certificates";
+// import Certificates from "./Components/Certificates";
 import Footer from "./Components/Footer";
 import ContactUs from './Components/ContactUs';
 
@@ -22,9 +22,9 @@ function App() {
         <ContactUs />
       </div>
 
-      <a style={{visibility: "hidden"}} name="certificates" href="www.puntayhacha.com.ar">contactus</a>
+      {/* <a style={{visibility: "hidden"}} name="certificates" href="www.puntayhacha.com.ar">contactus</a>
       <section id ="certificates"></section>
-        <Certificates />
+        <Certificates /> */}
         
       <a href="https://api.whatsapp.com/send?phone=5491163022062" className="btn-wsp" rel="noreferrer" target="_blank">
         <img className="app-wsp" src={wsp} alt="icon-wsp"/>
